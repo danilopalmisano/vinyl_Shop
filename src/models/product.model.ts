22be9f8f965cad4ac0bff01598b06273389d2cd5
Giadeite <giadanapoli94@gmail.com>
2024-04-08 16:43:00 +0200

    feature(product.model,line.model)"product.model,line.model"

---
+++ src/models/product.model.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { IProduct } from "../validation/product.validation";
+
+const ProductSchema = new mongoose.Schema<IProduct>(
+	{
+		name: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		price: {
+			type: Number,
+		},
+
+		images: [
+			{
+				type: String,
+			},
+		],
+
+		stockQuantity: {
+			type: Number,
+		},
+
+		stockStatus: {
+			type: String,
+			enum: ["in stock", "out of stock", "discontinued"],
+			default: "out of stock",
+		},
+		category: [
+			{
+				type: String,
+			},
+		],
+	},
+	{ timestamps: true }
+);
+
+export const Product = mongoose.model("Product", ProductSchema);
