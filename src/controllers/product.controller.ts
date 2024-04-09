d69e93d5ac2d893d79cd7f0d1f0c489a483ddc49
Danilo P <danilopupload@gmail.com>
2024-04-09 09:30:14 +0200

    feature(product.service, product.controller):"added basic function and service for product also test call with Request.rest"

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
-import { findProductById, getProducts } from "../services/product.service";
+import {
+	createProduct,
+	delProduct,
+	findProductById,
+	getProducts,
+	upProduct,
+} from "../services/product.service";
 
 export const showProducts = async (req: Request, res: Response) => {
 	try {
@@ -15,8 +21,18 @@ export const showSpecificProduct = async (req: Request, res: Response) => {
 	res.status(200).json(product);
 };
 
-export const addProduct = async (req, res) => {};
+export const addProduct = async (req: Request, res: Response) => {
+	const product = await createProduct(req.body);
+	res.status(201).json(product);
+};
 
-export const updateProduct = async (req, res) => {};
+export const updateProduct = async (req: Request, res: Response) => {
+	const _id = req.params.id;
+	const product = await upProduct(_id, req.body);
+	await res.status(200).json(product);
+};
 
-export const deleteProduct = async (req, res) => {};
+export const deleteProduct = async (req: Request, res: Response) => {
+	const product = await delProduct(req.params.id);
+	res.status(200).json({ message: "product deleted successfully" });
+};
